e439c6de2abba0c6a4c781b34eca9b666bab264d
Varun Iyer <vi.mail@protonmail.ch>
2017-03-26 02:37:42 -0700

    Added cull column removal

---
+++ tools/cull.py
@@ -0,0 +1,26 @@
+import sys
+
+cols = []
+if(len(sys.argv) >= 3):
+	csvfile = open(sys.argv[1])
+	cols = sys.argv[2].split(",")
+else:
+	cols = sys.argv[1].split(",")
+	csvfile = sys.stdin
+
+for idx, c in enumerate(cols):
+	cols[idx] = int(c)
+
+with csvfile as lines:
+	for l in lines:
+		lt = l.split(",")
+		#print(str(len(lt)))
+		nt = []
+		for idx, w in enumerate(lt):
+			if(cols.count(idx) == 0):
+				nt.append(w.strip("\""))
+	
+		print(",".join(nt))
+		
+	
+
