5a2b44f255f489b6c50d8635f128da9dd893aadd
Varun Iyer <vi.mail@protonmail.ch>
2017-03-26 01:02:14 -0700

    Updated column tool & added data framework

---
+++ data/README.md
@@ -0,0 +1,3 @@
+# Data
+Replace all rmi's (a thing I made up; redis mass insertion format) with the nppes data set wrapped in set commands.
+
--- tools/column.py
+++ tools/column.py
@@ -1,23 +1,25 @@
 import sys
 from prettytable import PrettyTable
-column = int(sys.argv[2])
-csvfile = sys.argv[1]
-	
 
-lines = []
-with open(csvfile) as f:
-    lines = f.read().splitlines()
+sysin = (len(sys.argv) <= 2)
 
+column = 0
+csvfile = ""
+if(sysin):
+	column = int(sys.argv[1])
+	csvfile = sys.stdin
+else:
+	column = int(sys.argv[2])
+	csvfile = open(sys.argv[1]) 
 types = dict({})
-for l in lines:
-	idx = 0
-	for i in range(column):
-	 	idx = l.index(",", idx) + 1
-	substr = l[idx:l.index(",", idx)]
-	if(not types.has_key(substr)):
-		types[substr] = 1
-	else:
-		types[substr] = types[substr] + 1
+with csvfile as f:
+	for l in f:
+		lt = l.split(",")
+		r = lt[column]
+		if(not types.has_key(r)):
+			types[r] = 1
+		else:
+			types[r] = types[r] + 1
 t = PrettyTable(['Column', 'Occurence'])
 for key, value in types.iteritems():
 	t.add_row([key, value])
