06de75ee45b304da7e60824a07f5be5f0ab5a405
DeadlyKnight510 <swimking510@gmail.com>
2017-03-26 00:39:55 -0700

    back end in place

---
+++ redis.php
@@ -0,0 +1,34 @@
+<?php
+ini_set('memory_limit', '2048M');
+require "predis/src/Autoloader.php";
+//require "RedisAutocomplete.php";
+Predis\Autoloader::register();
+// since we connect to default setting localhost
+// and 6379 port there is no need for extra
+// configuration. If not then you can specify the
+// scheme, host and port to connect as an array
+// to the constructor.
+try {
+    $redis = new Predis\Client();
+
+    $value = $redis->get("hello_world");
+//    echo $value;
+    
+}
+catch (Exception $e) {
+    echo "Couldn't connected to Redis";
+    echo $e->getMessage();
+}
+
+function get($input)
+{
+	$redis = new Predis\Client();
+	$arr = $input;
+	echo $arr;
+	$out = $redis->get($arr);
+	echo $out;
+}
+   if (isset($_POST['autocomplete'])) {
+        echo get($_POST['autocomplete']);
+   }
+?>
